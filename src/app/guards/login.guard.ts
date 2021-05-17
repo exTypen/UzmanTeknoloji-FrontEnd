054ee7646bd24da2e62a244a1c9f706a0530a02f
Yasir KILINÇ <63153220+exTypen@users.noreply.github.com>
2021-05-17 15:00:40 +0300

    Login Guard Added

---
+++ src/app/guards/login.guard.ts
@@ -0,0 +1,39 @@
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from './../services/auth.service';
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginGuard implements CanActivate {
+  constructor(
+    private authService: AuthService,
+    private toastrService: ToastrService,
+    private router: Router
+  ) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    if (this.authService.loggedIn()) {
+      return true;
+    } else {
+      this.router.navigate(['login']);
+      this.toastrService.error('Giriş Yapmalısınız', 'Hata');
+      return false;
+    }
+  }
+}
